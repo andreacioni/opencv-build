7c47c001c31f7b8efd24d18cd84d8c4c37ec1067
acioni <andreacioni@AndreaMacBookAir-2.local>
2018-01-24 23:32:59 +0100

    building

--- build-opencv.sh
+++ build-opencv.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-ls -l
+mkdir opencv/build
+cd opencv/build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+	-D CMAKE_INSTALL_PREFIX=/usr/local \
+	-D INSTALL_C_EXAMPLES=OFF \
+	-D INSTALL_PYTHON_EXAMPLES=ON \
+	-D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
+	-D BUILD_EXAMPLES=ON \
+-D ENABLE_NEON=ON ..
