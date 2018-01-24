a02b2786a175d8b91e312afd649d56a6b6e8c0c9
acioni <andreacioni@AndreaMacBookAir-2.local>
2018-01-24 23:10:49 +0100

    new dependency

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ./config.sh
 
+#Used for OpenCV
 sudo apt-get install -y build-essential cmake pkg-config
 sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
 sudo apt-get install -y libgtk2.0-dev libgstreamer0.10-0-dbg libgstreamer0.10-0 libgstreamer0.10-dev libv4l-0 libv4l-dev
@@ -15,6 +16,10 @@ sudo apt-get install -y v4l-utils
 sudo apt-get install -y python2.7 python2.7-dev python3 python3-dev
 sudo apt-get install -y oracle-java8-set-default ant
 
+#Used for cross-compiling
+apt-get install git rsync cmake ia32-libs
+
+#Intallink pip and numpy
 mkdir pip/
 cd pip
 wget https://bootstrap.pypa.io/get-pip.py
@@ -23,3 +28,9 @@ sudo python3 get-pip.py
 
 sudo pip install numpy
 sudo pip3 install numpy
+
+#Downloading OpenCV
+cd ..
+git clone https://github.com/opencv/opencv.git
+cd opencv
+git checkuot $OPENCV_VERSION
