715b0785f1dad07406251ea43c5d4fcdf45dc27f
Andrea <supercionci@hotmail.it>
2018-01-24 10:29:12 +0100

    travis setup

---
+++ install-dependencies.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+source config.sh
+
+sudo apt-get install -y build-essential cmake pkg-config
+sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get install -y libgtk2.0-dev libgstreamer0.10-0-dbg libgstreamer0.10-0 libgstreamer0.10-dev libv4l-0 libv4l-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y libatlas-base-dev gfortran
+sudo apt-get install -y python$PYTHON_2_VERSION python$PYTHON_3_VERSION
+sudo apt-get install -y python-numpy python-scipy python-matplotlib
+sudo apt-get install -y default-jdk ant
+sudo apt-get install -y libgtkglext1-dev
+sudo apt-get install -y v4l-utils
+sudo apt-get install -y python2.7 python2.7-dev python3 python3-dev
+sudo apt-get install -y oracle-java8-set-default
+
+mkdir pip/
+cd pip
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo python3 get-pip.py
+
+sudo pip install numpy
