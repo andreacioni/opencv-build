421e0ffb8b1481dd2a80be622ddb0333a6bead84
acioni <andreacioni@AndreaMacBookAir-2.local>
2018-01-31 22:56:40 +0100

    merge

--- build-opencv.sh
+++ build-opencv.sh
@@ -7,15 +7,9 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 	-D CMAKE_INSTALL_PREFIX=/usr/local \
 	-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
 	-D BUILD_EXAMPLES=OFF \
-<<<<<<< HEAD
 	#-DSOFTFP=ON \
 	-DCMAKE_TOOLCHAIN_FILE=../platforms/linux/arm-gnueabi.toolchain.cmake \
 	#-D ENABLE_NEON=ON ..
-=======
-	-DSOFTFP=ON \
-	-DCMAKE_TOOLCHAIN_FILE=../platforms/linux/arm-gnueabi.toolchain.cmake \
--D ENABLE_NEON=ON ..
->>>>>>> 6924ad0e26101b27abf716c8a87152e6438b6ecb
 
 make -j8
 
--- install-dependencies.sh
+++ install-dependencies.sh
@@ -8,20 +8,14 @@ sudo apt-get install -y libgtk2.0-dev libgstreamer0.10-0-dbg libgstreamer0.10-0 
 sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install -y libxvidcore-dev libx264-dev
 sudo apt-get install -y libatlas-base-dev gfortran
-<<<<<<< HEAD
 sudo apt-get install -y python$PYTHON_2_VERSION python$PYTHON_3_VERSION
 #sudo apt-get install -y python-numpy python-scipy python-matplotlib
-=======
-sudo apt-get install -y python2 python3
-sudo apt-get install -y python-numpy python-scipy python-matplotlib
->>>>>>> 6924ad0e26101b27abf716c8a87152e6438b6ecb
 #sudo apt-get install -y default-jdk ant
 sudo apt-get install -y libgtkglext1-dev
 sudo apt-get install -y v4l-utils
 sudo apt-get install -y python2.7 python2.7-dev python3 python3-dev
 sudo apt-get install -y oracle-java8-set-default ant
 
-<<<<<<< HEAD
 #Setting up tools for cross compilation (armv6, RPi 1/Zero)
 #cd ..
 #git clone https://github.com/raspberrypi/tools.git --depth 1
@@ -32,14 +26,6 @@ sudo apt-get install -y oracle-java8-set-default ant
 
 #Used for cross-compiling (armv7, RPi 2/3)
 sudo apt-get install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
-=======
-#Used for .deb creation
-sudo apt-get install build-essential automake autoconf libtool pkg-config libcurl4-openssl-dev intltool libxml2-dev libgtk2.0-dev libnotify-dev libglib2.0-dev libevent-dev checkinstall
-
-#Used for cross-compiling
-sudo apt-get install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
-#git clone https://github.com/raspberrypi/tools.git
->>>>>>> 6924ad0e26101b27abf716c8a87152e6438b6ecb
 
 #Intallink pip and numpy
 mkdir pip/
