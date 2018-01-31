6b0d1c40cd510d66d87e893d94c08ab0b83914b6
acioni <andreacioni@AndreaMacBookAir-2.local>
2018-01-31 22:55:15 +0100

    retry

--- build-opencv.sh
+++ build-opencv.sh
@@ -5,12 +5,10 @@ cd opencv/build
 
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
 	-D CMAKE_INSTALL_PREFIX=/usr/local \
-	-D INSTALL_C_EXAMPLES=OFF \
-	-D INSTALL_PYTHON_EXAMPLES=OFF \
 	-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
 	-D BUILD_EXAMPLES=OFF \
 	#-DSOFTFP=ON \
-	#-DCMAKE_TOOLCHAIN_FILE=../platforms/linux/arm-gnueabi.toolchain.cmake \
--D ENABLE_NEON=ON ..
+	-DCMAKE_TOOLCHAIN_FILE=../platforms/linux/arm-gnueabi.toolchain.cmake \
+	#-D ENABLE_NEON=ON ..
 
 make -j7
--- install-dependencies.sh
+++ install-dependencies.sh
@@ -9,16 +9,23 @@ sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install -y libxvidcore-dev libx264-dev
 sudo apt-get install -y libatlas-base-dev gfortran
 sudo apt-get install -y python$PYTHON_2_VERSION python$PYTHON_3_VERSION
-sudo apt-get install -y python-numpy python-scipy python-matplotlib
+#sudo apt-get install -y python-numpy python-scipy python-matplotlib
 #sudo apt-get install -y default-jdk ant
 sudo apt-get install -y libgtkglext1-dev
 sudo apt-get install -y v4l-utils
 sudo apt-get install -y python2.7 python2.7-dev python3 python3-dev
 sudo apt-get install -y oracle-java8-set-default ant
 
-#Used for cross-compiling
-sudo apt-get install gcc-arm-linux-gnueabihf
-#git clone https://github.com/raspberrypi/tools.git
+#Setting up tools for cross compilation (armv6, RPi 1/Zero)
+#cd ..
+#git clone https://github.com/raspberrypi/tools.git --depth 1
+#sudo ln -s ./tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-gcc /usr/bin/rpi-gcc
+#sudo ln -s ./tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-g++ /usr/bin/rpi-g++
+#sudo ln -s ./tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-ar /usr/bin/rpi-ar
+#sudo ln -s ./tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin/arm-linux-gnueabihf-ranlib /usr/bin/rpi-ranlib
+
+#Used for cross-compiling (armv7, RPi 2/3)
+sudo apt-get install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
 
 #Intallink pip and numpy
 mkdir pip/
@@ -40,3 +47,4 @@ cd ..
 git clone https://github.com/opencv/opencv_contrib.git
 cd opencv_contrib
 git checkout $OPENCV_CONTRIB_VERSION
+
